2c9dbd666557ecf6e504c71674f65a1aa8866e72
Bilel Marzouki <bilelmarzou@gmail.com>
2026-02-25 15:40:06 +0100

    done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const logger = require("morgan");
-
+const mongoose = require("mongoose");
 const app = express();
 
 // MIDDLEWARE
@@ -11,6 +11,12 @@ app.use(express.json());
 
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
+const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
+mongoose
+  .connect(MONGODB_URI)
+  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to mongo", err));
+
 
 
 
@@ -20,26 +26,65 @@ app.get('/', (req, res) => {
     res.send("<h1>LAB | Express Mongoose Recipes</h1>");
 });
 
+const Recipe = require("./models/Recipe.model")
 
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
-
+app.post("/recipes", async(req, res)=>{
+    try {
+        await Recipe.create(req.body)
+        console.log(req.body)
+        res.send("add recipe")
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
-
-
+app.get("/recipes", async(req, res)=>{
+    try {
+        const response = await Recipe.find()
+        res.json(response)
+    } catch (error) {
+        console.log(error)
+    }
+})
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
-
+app.get("/recipes/:id", async(req, res)=>{
+    try {
+        const {id} =req.params
+        const response = await Recipe.findById(id)
+        res.json(response)
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
-
+app.put('/recipes/:id', async(req, res)=>{
+    try {
+        const {id}= req.params
+        const response = await Recipe.findByIdAndUpdate(id ,req.body, {new: true})
+        res.json(response)
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 //  Iteration 7 - Delete a Single Recipe
 //  DELETE  /recipes/:id route
-
+app.delete("/recipes/:id",async(req, res)=>{
+    try {
+        const {id}= req.params
+        await Recipe.findByIdAndDelete(id)
+        res.send("deleted successfully")
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 
 // Start the server
